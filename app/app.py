1b23122300793f74c31bdfe2a63749543c2077b3
2500030581-chinnamvinayreddy <vinaychinna55@gmail.com>
2026-09-16 12:44:24 +0530

    Updated files.

--- app/app.py
+++ app/app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 from src.data.load_data import load_data, get_summary
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def home():
+
     return render_template("home.html")
+
 
 @app.route("/dataset")
 def dataset():
@@ -22,11 +26,52 @@ def dataset():
         cols=summary["columns"],
         target=summary["target"],
         columns=df.columns.tolist(),
-        data=df.head(10).values.tolist()   # Display first 10 rows
+        data=df.head(10).values.tolist()
     )
+
+
 @app.route("/eda")
 def eda():
+
     return render_template("eda.html")
+
+
+@app.route("/preprocessing")
+def preprocessing():
+
+    # Load dataset
+    df = load_data()
+
+    return render_template(
+        "preprocessing.html",
+        rows=df.shape[0],
+        cols=df.shape[1]
+    )
+
+
+@app.route("/models")
+def models():
+
+    return render_template("models.html")
+
+
+@app.route("/evaluation")
+def evaluation():
+
+    return render_template("evaluation.html")
+
+
+@app.route("/comparison")
+def comparison():
+
+    return render_template("comparison.html")
+
+
+@app.route("/prediction")
+def prediction():
+
+    return render_template("prediction.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
